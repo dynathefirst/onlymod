a51cb39bb5be14b27439833928375917299a95c3
Dyna-Minecraft <ryxcory@gmail.com>
2023-11-27 15:49:44 +0000

    0.5.0

--- src/main/java/net/dyna/onlymod/TheOnlyModYouWillEverNeedClient.java
+++ src/main/java/net/dyna/onlymod/TheOnlyModYouWillEverNeedClient.java
@@ -5,5 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class TheOnlyModYouWillEverNeedClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+
     }
 }
